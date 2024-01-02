97d2709c627bc8e39587fb7aaa77d5de661c3d62
Omprakash  D <omugan07@gmail.com>
2024-01-02 20:16:28 +0530

    fixed bugs

--- src/pages/Technology/Technology.jsx
+++ src/pages/Technology/Technology.jsx
@@ -31,12 +31,12 @@ const Technology = () => {
       <Card className="mx-6 sm:mx-32 max-sm:mb-8 my-4 bg-gray-200 border-0 shadow-lg">
         <CardHeader className="p-4 px-8">
           <CardTitle className="text-xl md:text-2xl font-quando text-primary flex">
-            <IoIosFastforward size={24} className="text-primary mt-1" />
+            <IoIosFastforward size={24} className="text-primary sm:mt-1" />
             <span className="ml-2">Fast Grading</span>
           </CardTitle>
         </CardHeader>
         <CardContent className="px-8 pb-4">
-          <CardDescription className="text-base lg:text-lg text-black/75 font-oxygen">
+          <CardDescription className="text-sm sm:text-base lg:text-lg text-black/75 font-oxygen">
             Li-Circle's fast grading allows you to analyze and understand
             batteries State of Health and maximize their remaining useful life.
           </CardDescription>
@@ -46,14 +46,14 @@ const Technology = () => {
       <Card className="mx-6 sm:mx-32 max-sm:mb-8 my-4 bg-gray-200 border-0 shadow-lg">
         <CardHeader className="p-4 px-8">
           <CardTitle className="text-xl md:text-2xl font-quando text-primary flex">
-            <IoIosFastforward size={24} className="text-primary mt-1" />
+            <IoIosFastforward size={24} className="text-primary sm:mt-1" />
             <span className="ml-2">
               Extract maximum value from used batteries
             </span>
           </CardTitle>
         </CardHeader>
         <CardContent className="px-8 pb-4">
-          <CardDescription className="text-base md:text-lg text-black/75 font-oxygen">
+          <CardDescription className="text-sm sm:text-base md:text-lg text-black/75 font-oxygen">
             Repurposing EV batteries doesn't have to be confusing. Li-Circle can
             help you determine the appropriate use for each battery.
           </CardDescription>
